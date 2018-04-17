2dd8b3663341fad29b0cfc24570165b5e5756709
Christian Drengenberg <Christian.Drengenberg@opuscapita.com>
2018-04-17 12:19:55 +0200

    Made queueName configuration value mandatory.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const cachedInstances = { };
 /**
  * Options object for event emmitting.
  * @typedef {Object} EmitOpts
- * @property {boolean} mandatory If true, the event will be returned if it is not routed to a queue.
  * @property {number} expiration Time in milliseconds for an event to last in a queue before it gets removed.
  */
 
@@ -20,6 +19,13 @@ const cachedInstances = { };
  * @typedef {Object} SubscribeOpts
  * @property {number} messageLimit The maximum amount of unacknowleged messages a single subscription will get at once.
  */
+
+ /**
+  * Options object for queue delition.
+  * @typedef {Object} DeleteQueueOpts
+  * @property {boolean} unusedOnly Only deletes a queue if it has no consumers.
+  * @property {boolean} emptyOnly Only deletes a queue if it does not contain any messages.
+  */
 
 /**
 * Class for simplifying access to message queue servers implementing the Advanced Message Queuing Protocol (amqp).
@@ -47,9 +53,12 @@ class EventClient
         this.callbacks = { };
         this.serviceName = configService.serviceName;
         this.exchangeName = this.config.exchangeName || this.serviceName;
-        this.queueName = this.config.queueName || this.serviceName;
+        this.queueName = this.config.queueName;
         this.logger = new Logger({ context : { serviceName : this.serviceName } });
         this.callbackErrorCount = { };
+
+        if(!this.queueName)
+            throw new Error('Required configuration value "queueName" has not been provided.');
 
         cachedInstances[cacheKey] = this;
     }
@@ -58,7 +67,7 @@ class EventClient
      * Makes some basic initializations like exchange creation as they are automatically done by emitting the first event.
      * This is used to create the required environment without pushing anything tho the queue.
      *
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with true if the subscription succeeded. Otherwise the promise gets rejected with an error.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to true if the subscription succeeded. Otherwise the promise gets rejected with an error.
      */
     init()
     {
@@ -78,7 +87,7 @@ class EventClient
      * @param {object} message - Payload to be sent to a receiver.
      * @param {object} context - Optional context containing meta data for the receiver of an event.
      * @param {EmitOpts} opts - Additional options to be set for emmiting an event.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with null if the subscription succeeded. Otherwise the promise gets rejected with an error.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to null if the subscription succeeded. Otherwise the promise gets rejected with an error.
      */
     emit(topic, message, context = null, opts = { })
     {
@@ -112,7 +121,7 @@ class EventClient
             appId : this.serviceName,
             messageId : messageId,
             headers : transportObj.context
-        }
+        };
 
         logger.contextify(extend(true, { }, transportObj.context, options));
         logger.info(`Emitting event "${topic}"`);
@@ -138,7 +147,7 @@ class EventClient
      * @param {string} topic - Full name of a topic or a pattern.
      * @param {function} callback - Function to be called when a message for a topic or a pattern arrives.
      * @param {SubscribeOpts} opts - Additional options to set for the subscription.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with null if the subscription succeeded. Otherwise the promise gets rejected with an error.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to null if the subscription succeeded. Otherwise the promise gets rejected with an error.
      */
     subscribe(topic, callback, opts = { })
     {
@@ -220,7 +229,7 @@ class EventClient
      * Checks whenever an exchange exists or not. As checking for non existing exchnages provokes server errors (404) that will destroy the communication channel and log an error, this method should
      * not be called excessively.
      * @param {string} exchangeName - The name of the exchange to find.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with true or false depending on whenever the exchange exists.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to true or false depending on whenever the exchange exists.
      */
     exchangeExists(exchangeName)
     {
@@ -231,7 +240,7 @@ class EventClient
      * Checks whenever a queue exists or not. As checking for non existing queues provokes server errors (404) that will destroy the communication channel and log an error, this method should
      * not be called excessively.
      * @param {string} queueName - The name of the queue to find.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with true or false depending on whenever the queue exists.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to true or false depending on whenever the queue exists.
      */
     queueExists(queueName)
     {
@@ -239,10 +248,30 @@ class EventClient
     }
 
     /**
+     * Removes a queue if it exists.
+     * @param {string} queueName - The name of the queue to remove.
+     * @param {DeleteQueueOpts} opts - Additional options to set for delition.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) always resolving to null. It only gets rejected in case the server connection could not be estabished.
+     */
+    deleteQueue(queueName, opts = { })
+    {
+        opts = { ifUnused : opts.unusedOnly, ifEmpty : opts.emptyOnly };
+
+        return Promise.resolve(this._getNewChannel()).then(async channel =>
+        {
+            await channel.deleteQueue(queueName, opts).catch(e => null);
+            await channel.close().catch(e => null);
+            await this.disposeSubscriber().catch(e => null);
+
+            return null;
+        });
+    }
+
+    /**
      * This method allows you to unsubscribe from a previous subscribed *topic* or pattern.
      *
      * @param {string} topic - Full name of a topic or a pattern.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with true or false depending on whenever the topic existed in the subscriptions.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to true or false depending on whenever the topic existed in the subscriptions.
      */
     unsubscribe(topic)
     {
@@ -268,7 +297,7 @@ class EventClient
 
     /**
      * Method for releasing the publishing channel.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with true or false if there was no active connection.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to true or false if there was no active connection.
      */
     disposePublisher()
     {
@@ -280,7 +309,7 @@ class EventClient
 
     /**
      * Method for releasing all subscriptions and close the subscription channel.
-     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving with true or false if there was no active connection.
+     * @returns {Promise} [Promise](http://bluebirdjs.com/docs/api-reference.html) resolving to true or false if there was no active connection.
      */
     disposeSubscriber()
     {
