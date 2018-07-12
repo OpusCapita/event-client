05fe56280c0d4128f6a833142a35e033c6356bba
Dennis Bücker <dennis.buecker@opuscapita.com>
2018-07-12 09:49:08 +0200

    Working on stabalizing EventClient
    + Event based dependency between connection and channel for proper error
    handling
    + State management
    + Switched to confirmation mode for pubChannel
    + Added local cache of unacked messages in confirmation mode

--- local/debug.js
+++ local/debug.js
@@ -5,39 +5,64 @@ const {EventClient} = require('../lib');
 // const sleep = (millis) => new Promise(resolve => setTimeout(resolve, millis));
 
 async function main() {
-    await configService.init({logger : Logger.DummyLogger});
+    await configService.init({logger: Logger.DummyLogger});
 
     const config = EventClient.DefaultConfig.consul;
     const endpoint = await configService.getEndPoint(config.mqServiceName);
     const username = config.mqUserKey && await configService.get(config.mqUserKey);
     const password = config.mqPasswordKey && await configService.get(config.mqPasswordKey);
 
-    const consulOverride = { };
+    const consulOverride = {};
     consulOverride.host = endpoint.host;
     consulOverride.port = endpoint.port;
     consulOverride.username = username;
     consulOverride.password = password;
 
     const client = new EventClient({logger: new Logger()});
+    global.ec = client;
 
-    const routingKey = 'event-client.Test';
+    const routingKey1 = 'event-client.testone';
+    const routingKey2 = 'event-client.testtwo';
 
+    let rxTx = [];
+
+
+    // Also works w/o call to init
     await client.init();
 
-    await client.subscribe(routingKey, async (payload, context, key) => {
+    await client.subscribe(routingKey1, async (payload) => {
+        console.log('Received event on ' + routingKey1);
         console.log(payload);
+
+        rxTx.pop();
 
         return true;
     });
 
-    global.ec = client;
 
-    setInterval( async () => {
+    await client.subscribe(routingKey2, async (payload) => {
+        console.log('Received event on ' + routingKey2);
+        console.log(payload);
+
+        rxTx.pop();
+
+        return true;
+    });
+
+    setInterval(async () => {
         console.log('Emitting event ...');
 
-        let result = await client.emit(routingKey, {message: new Date()});
+        let result = await client.emit(routingKey1, {message: new Date()});
         // TODO result should be false or throw if connection is blocked
-    }, 30000);
+        //
+        //
+        rxTx.push(result);
+
+        console.log(`Emit result is: ${result}`);
+        console.log(`RxTx size = ${rxTx.length}`);
+    }, 5000);
+    
+    return true;
 }
 
 try {
@@ -45,4 +70,5 @@ try {
 } catch (e) {
     /* handle error */
     console.log(e);
+    debugger;
 }
